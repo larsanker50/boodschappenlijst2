acb41cca17ca2765e0b580dd17a0f492ff6dea42
larsanker50 <75078152+larsanker50@users.noreply.github.com>
2021-01-08 15:53:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,26 @@
-const inputBrood = document.getElementsByName('brood')[0];
-const inputBrocolli = document.getElementsByName('brocolli')[0];
-const inputKrentenbollen = document.getElementsByName('krentenbollen')[0];
-const inputNoten = document.getElementsByName('noten')[0];
+const productPrice = document.getElementsByClassName('productPrice');
+const productQuantity = document.getElementsByClassName('productQuantity');
+const productTotalCost = document.getElementsByClassName('productTotalCost');
+const totalCost = document.getElementById('totalCost');
 
-const subTotaalBrood = document.getElementsByClassName('productTotalCostA')[0];
-const subTotaalBrocolli = document.getElementsByClassName('productTotalCostB')[0];
-const subTotaalKrentenbollen = document.getElementsByClassName('productTotalCostC')[0];
-const subTotaalNoten = document.getElementsByClassName('productTotalCostD')[0];
-const subTotaal = document.getElementsByClassName('totalCost')[0];
-
-inputBrood.addEventListener('change',() => {
-    updateValueBrood();
-    updateTotalCost();
+productQuantity.innerHTML.addEventListner('change',() => { //deze werkt nog niet
+    update(); //deze werkt nog niet
 });
 
-inputBrocolli.addEventListener('change',() => {
-    updateValueBrocolli();
-    updateTotalCost();
-});
+function loop() {
+    for (i = 0; i < 4; i++) {
+    
+    
+        productPrice[i].innerHTML;
+    }
 
-inputKrentenbollen.addEventListener('change',() => {
-    updateValueKrentenbollen();
-    updateTotalCost();
-});
+};
 
-inputNoten.addEventListener('change',() => {
-    updateValueNoten();
-    updateTotalCost();
-});
+function update() {
+    console.log('test')
+};
 
 
-function updateValueBrood() {
-    math = (1 * +inputBrood.value);
-    subTotaalBrood.innerHTML = math;
-    return math;
-}
-
-function updateValueBrocolli() {
-    math = (0.99 * +inputBrocolli.value);
-    subTotaalBrocolli.innerHTML = math;
-    return math;
-}
-
-function updateValueKrentenbollen() {
-    math = (1.20 * +inputKrentenbollen.value);
-    subTotaalKrentenbollen.innerHTML = math;
-    return math;
-}
-
-function updateValueNoten() {
-    math = (2.99 * +inputNoten.value);
-    subTotaalNoten.innerHTML = math;
-    return math;
-}
-
-function updateTotalCost() {
-    math = (+updateValueBrood() + +updateValueBrocolli() + +updateValueKrentenbollen() + +updateValueNoten());
-    subTotaal.innerHTML = math;
-    return math;
-}
+//for (i = 0; i < 4; i++) {
+//    console.log(productPrice[i].innerHTML);
+//};
