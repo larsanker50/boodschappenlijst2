e972fa0083c5b62680d976281e6e5aa630386e38
larsanker50 <75078152+larsanker50@users.noreply.github.com>
2021-01-08 13:48:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,59 @@ const inputBrocolli = document.getElementsByName('brocolli')[0];
 const inputKrentenbollen = document.getElementsByName('krentenbollen')[0];
 const inputNoten = document.getElementsByName('noten')[0];
 
+const subTotaalBrood = document.getElementsByClassName('productTotalCostA')[0];
+const subTotaalBrocolli = document.getElementsByClassName('productTotalCostB')[0];
+const subTotaalKrentenbollen = document.getElementsByClassName('productTotalCostC')[0];
+const subTotaalNoten = document.getElementsByClassName('productTotalCostD')[0];
+const subTotaal = document.getElementsByClassName('totalCost')[0];
 
-inputBrood.addEventListener('change', updateValueBrood)
-inputBrocolli.addEventListener('change', updateValueBrocolli)
-inputKrentenbollen.addEventListener('change', updateValueKrentenbollen)
-inputNoten.addEventListener('change', updateValueNoten)
+inputBrood.addEventListener('change',() => {
+    updateValueBrood();
+    updateTotalCost();
+});
+
+inputBrocolli.addEventListener('change',() => {
+    updateValueBrocolli();
+    updateTotalCost();
+});
+
+inputKrentenbollen.addEventListener('change',() => {
+    updateValueKrentenbollen();
+    updateTotalCost();
+});
+
+inputNoten.addEventListener('change',() => {
+    updateValueNoten();
+    updateTotalCost();
+});
 
 
-
-function updateValueBrood(i){
-    document.getElementsByClassName('cost')[0].addEventListener //niet af hier beginnen
+function updateValueBrood() {
+    math = (1 * +inputBrood.value);
+    subTotaalBrood.innerHTML = math;
+    return math;
 }
 
-function updateValueBrocolli(i){
-    console.log("brocollitest is goed")
+function updateValueBrocolli() {
+    math = (0.99 * +inputBrocolli.value);
+    subTotaalBrocolli.innerHTML = math;
+    return math;
 }
 
-function updateValueKrentenbollen(i){
-    console.log("krentenbollentest is goed")
+function updateValueKrentenbollen() {
+    math = (1.20 * +inputKrentenbollen.value);
+    subTotaalKrentenbollen.innerHTML = math;
+    return math;
 }
 
-function updateValueNoten(i){
-    console.log("notentest is goed")
+function updateValueNoten() {
+    math = (2.99 * +inputNoten.value);
+    subTotaalNoten.innerHTML = math;
+    return math;
+}
+
+function updateTotalCost() {
+    math = (+updateValueBrood() + +updateValueBrocolli() + +updateValueKrentenbollen() + +updateValueNoten());
+    subTotaal.innerHTML = math;
+    return math;
 }
